fb0fabb2f1ba00878c24637288774f1a041256ef
Fardeen Tariq <95141253+iwaai@users.noreply.github.com>
2023-02-07 10:52:23 +0500

    Create sai.py

---
+++ sai.py
@@ -0,0 +1,73 @@
+SWITCH: This device usually has 24 ports with each port having the
+ability to learn 132 mac addresses
+
+
+White/Green
+Green
+White/Orange
+Blue
+White/Blue
+Orange
+White/Brown
+
+
+int swetiport mode acess
+swirtcjport port-security
+switchport port-security maximun 4
+switport port-security violation shut
+
+
+
+
+acess-list 1 deny network wildcard
+acess-list 1 permit any
+sh access-list
+
+int fa0/0
+ip access-group 1 out
+
+
+
+
+router rip
+router eigrp 1
+router ospf 1
+nwtwork jo pata h
+
+network 192.168.1.0 0.0.0.255 area 1
+network 192.168.2.0 0.0.0.255 area 0
+exit
+
+--------------------------------------
+
+pehle vlan bnao
+phr interface vlan 10
+ip addr jo deni ha + subnet
+IP ROUTING
+
+
+
+
+
+vlan bnao
+int bazi
+router mein enable kro port ko
+switchport mode trunk	 
+exit
+
+interface fa0/0.1
+encapsulation dot1q 10
+ip add ... sub
+
+exit
+
+int fa0/0.2
+encap   dot1Q 20
+ip add 
+
+
+
+
+
+
+
